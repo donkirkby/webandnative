a3e211f8a0047521c0bedc3b240543ae95ae9ad1
Don Kirkby <donkirkby@gmail.com>
2014-12-14 09:24:50 -0800

    Remove server-side code from GWT project.
    Replace with "Hello, World!" example.
    Rename Webandnative_html.html to index.html.
    Rename EntryPoint class.

---
+++ html/src/com/github/donkirkby/webandnative/client/WebAndNativeEntryPoint.java
@@ -0,0 +1,46 @@
+package com.github.donkirkby.webandnative.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class WebAndNativeEntryPoint implements EntryPoint {
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+        final Button nextButton = new Button("Next");
+        final Label puzzleLabel = new Label();
+        puzzleLabel.setText("Hello, World!");
+        final Label errorLabel = new Label();
+
+        // Add the nameField and sendButton to the RootPanel
+        // Use RootPanel.get() to get the entire body element
+        RootPanel.get("puzzleLabelContainer").add(puzzleLabel);
+        RootPanel.get("nextButtonContainer").add(nextButton);
+        RootPanel.get("errorLabelContainer").add(errorLabel);
+
+        // Focus the cursor on the name field when the app loads
+        nextButton.setFocus(true);
+
+        // Create a handler for the sendButton and nameField
+        class MyHandler implements ClickHandler {
+            /**
+             * Fired when the user clicks on the sendButton.
+             */
+            public void onClick(ClickEvent event) {
+                puzzleLabel.setText("Good-bye.");
+            }
+        }
+
+        // Add a handler to send the name to the server
+        MyHandler handler = new MyHandler();
+        nextButton.addClickHandler(handler);
+    }
+}
